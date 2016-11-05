3ad527d62fe8b65287d9f4830a9eab4ea81f911b
giveme5 <286674783@qq.com>
2016-11-05 13:53:59 +0800

    Initial Commit

---
+++ HTTPMethodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPMethodTest
+//
+//  Created by n on 16/11/5.
+//  Copyright © 2016年 summerwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPMethodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPMethodTest
+//
+//  Created by n on 16/11/5.
+//  Copyright © 2016年 summerwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
